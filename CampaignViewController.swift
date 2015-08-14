06380e595cb390592439d1bd9acfd29a451b8bfb
Leonardo Reis <leo.reis@b2wdigital.com>
2015-08-14 16:17:27 -0300

    Initial Commit

---
+++ CampaignViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CampaignViewController.swift
+//  CrowdVoucher
+//
+//  Created by Leonardo Reis on 8/14/15.
+//  Copyright (c) 2015 Leonardo Reis. All rights reserved.
+//
+
+import UIKit
+
+class CampaignViewController: UITableViewController {
+
+}
---
+++ ShareCampaignViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ShareCampaignViewController.swift
+//  CrowdVoucher
+//
+//  Created by Leonardo Reis on 8/14/15.
+//  Copyright (c) 2015 Leonardo Reis. All rights reserved.
+//
+
+import UIKit
+
+class ShareCampaignViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
